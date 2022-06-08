4cf5cf422cec52961e3439b5075524f3c913eb03
Pilvista <pilvosta@gmail.com>
2022-06-09 03:14:11 +0600

    Learned more

---
+++ forloop.js
@@ -0,0 +1,9 @@
+// for(var i = 0; i<=10; i++){
+// console.log(i);
+// }
+
+var ageList = [12, 66, 78, 33, 25, 67, 56];
+for( var i = 0; i < ageList.length; i++){
+    var elements =ageList[i];
+console.log(elements);
+}
---
+++ switch.js
@@ -0,0 +1,14 @@
+var num = 10;
+switch(num){
+    case 100:
+        console.log("You are Out");
+    break;
+    case 50:
+        console.log("I am out");
+    break;
+    case 10:
+        console.log("I am Alive");
+    break;
+    default:
+        console.log(" Who are you?")
+}
